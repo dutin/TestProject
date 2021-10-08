ab3c37b73d9b2dc7640049285cfe93021c8f32e0
daniel.dutin <daniel.dutin@scalefocus.com>
2021-10-08 18:20:28 +0300

    first commit

---
+++ TestProject/Steps/Service2/Step2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Steps.Service2
+{
+    class Step2
+
+    {
+    }
+}
