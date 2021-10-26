857343c1b9a720c03ad37cdc45cbe3f54af0b29a
daniel.dutin <daniel.dutin@scalefocus.com>
2021-10-26 14:10:27 +0300

    Latest Task Related to automaiton tests

---
+++ TestProject/Steps/GetRolesByIDSteps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.DTOs;
+using TestProject.Steps.BaseSteps;
+
+namespace TestProject.Steps.BaseSteps
+{
+    public class GetRolesByIDSteps : BaseGetSteps<List<RoleDTO>>
+    {
+        public GetRolesByIDSteps(Uri baseUrl, string localToken) : base(baseUrl, localToken, "/roles") { }
+    }
+}
